1afdf16efa17ca42c41e2acd1d34d28d7aa03bce
Vik Sir <vsirotkin15@gmail.com>
2024-03-19 12:14:11 +0300

    footer

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,12 +1,15 @@
 <script>
 	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
 <Header />
 
 <main>
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio explicabo blanditiis voluptas, neque deleniti placeat maxime odio, veritatis hic, sed fugiat totam corrupti quidem temporibus nemo? Dolorum in corrupti quod.</p>
+	
 </main>
+
+<Footer />
 
 <style>
 	main {
