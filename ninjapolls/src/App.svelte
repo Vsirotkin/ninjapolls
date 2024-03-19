ec3c681b37e9affb908f6734491ef466442947bd
Vik Sir <vsirotkin15@gmail.com>
2024-03-19 13:48:20 +0300

    active change between two tabs

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,12 +1,23 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	// tabs
+	let items = [
+		'Current Polls',
+		'Add New Poll'
+	];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 
 <Header />
 
 <main>
-	
+	<Tabs {activeItem} {items} on:tabChange={tabChange}/>
 </main>
 
 <Footer />
