ece36c2425d6ac1f6f4d525452f798c53ecc790a
Vik Sir <vsirotkin15@gmail.com>
2024-03-20 12:10:57 +0300

    poll_list setup

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,7 +1,10 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import PollList from "./components/PollList.svelte";
+
   import CreatePollForm from "./components/CreatePollForm.svelte";
+
   import Tabs from "./shared/Tabs.svelte";
 
   // tabs
@@ -37,7 +40,7 @@
 <main>
   <Tabs {activeItem} {items} on:tabChange={tabChange} />
   {#if activeItem === "Current Polls"}
-    <p>Poll list component goes here</p>
+    <PollList {polls} />
   {:else if activeItem === "Add New Poll"}
     <CreatePollForm on:add={handleAdd} />
 
