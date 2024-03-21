800b1d6df9bd20d03dda762e47ee98975d7c666d
Vik Sir <vsirotkin15@gmail.com>
2024-03-21 12:23:44 +0300

    setup store to keep data

--- ninjapolls/src/components/PollList.svelte
+++ ninjapolls/src/components/PollList.svelte
@@ -1,17 +1,27 @@
 <script>
+  // import { onMount, onDestroy } from 'svelte';
   import PollStore from '../stores/PollStore.js';
   import PollDetails from "./PollDetails.svelte";
 
-  export let polls = [];
+  // export let polls = [];
 
   // get data from PollStore and re-render it in poll-list
-  PollStore.subscribe(data => {
-    polls = data;
-  });
+  // const unsub = PollStore.subscribe(data => {
+  //   polls = data;
+  // });
 </script>
 
+<!-- onMount(() => {
+  console.log('components mounted');
+});
+
+on Destroy(() => {
+  console.log('component destroyed');
+  unsub();
+}); -->
+
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  {#each $PollStore as poll (poll.id)}
     <div>
       <PollDetails {poll} on:vote />
     </div>
